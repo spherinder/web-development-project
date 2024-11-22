2646bf5426b701c969bda52add08b4e6243ca509
Xinglu Chen <public@yoctocell.xyz>
2024-11-22 16:50:54 +0100

    api: Add some dummy endpoints

--- api/src/__init__.py
+++ api/src/__init__.py
@@ -1,9 +1,17 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def hello_world() -> Response:
     data = {"content" : "Hellow"}
     app.logger.debug('A value for debugging')
     return jsonify(data)
+
+@app.route("/markets")
+def getMarkets() -> Response:
+    pass
+
+@app.route("/market/<name>")
+def getMarket() -> Response:
+    pass
