4a4d2ac5e60671bee3a7f1bfa17c054b1c2af950
Xinglu Chen <public@yoctocell.xyz>
2024-12-03 16:49:07 +0100

    api: Add support for CORS

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 from config import Config
 
 db = SQLAlchemy()
@@ -9,6 +10,7 @@ migrate = Migrate()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
+    CORS(app, supports_credentials=True)
     app.config.from_object(config_class)
     db.init_app(app)
     migrate.init_app(app, db)
