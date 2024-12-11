044df99ef48b933843f6269e71140677b5831492
Teymour Aldridge <teymour@reasoning.page>
2024-12-11 22:37:33 +0100

    set env var

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,3 @@
+export SQLALCHEMY_DATABASE_URI="/http/app.db"
 python -m flask db upgrade
-gunicorn --config gunicorn_config.py app:app
+gunicorn --config gunicorn_config.py app:app --env SQLALCHEMY_DATABASE_URI="/http/app.db"
